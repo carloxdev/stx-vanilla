4811af94f078f106a6cacb513eeb5538e8490a44
carloxdev <carloxdev@gmail.com>
2019-02-16 00:51:34 -0600

    Se agregan nuevos componentes

---
+++ src/CardFormImage/index.js
@@ -0,0 +1,33 @@
+import './styles.css'
+
+export default class Image {
+
+    constructor (_btn_image) {
+        this.button_load = _btn_image
+        this.preview = document.getElementById("imagecard-body-img")
+        this.file_file = document.getElementById(
+            this.button_load.getAttribute("for")
+        )
+        this.init_Events()
+    }
+
+    init_Events() {
+        this.file_file.addEventListener(
+            "change",
+            this.click_ButtonLoad.bind(this)
+        )
+    }
+
+    click_ButtonLoad(e) {
+        if (this.file_file.files && this.file_file.files.item(0)) {
+
+            let reader = new FileReader()
+
+            var _this = this
+            reader.onload = function (event) {
+                _this.preview.setAttribute("src", event.target.result)
+            }
+            reader.readAsDataURL(this.file_file.files.item(0))
+        }
+    }
+}
